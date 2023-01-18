cf44a8d02816deeca0bb96bfbf237f645b14f49d
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-01-18 05:22:38 -0700

    Destructuring

--- JavaScript Refresher/destructuring.js
+++ JavaScript Refresher/destructuring.js
@@ -8,3 +8,10 @@ console.log(b); //Gil
 const {name} = {name:'Gil',age:26}
 console.log(name) //Gil
 console.log(age) //undefined
+
+
+const numbers = [1,2,3];
+[num1, num2] = numbers;
+console.log(num1,num2)//1,2
+[num1, ,num3] = numbers;
+console.log(num1,num3)//1,3
