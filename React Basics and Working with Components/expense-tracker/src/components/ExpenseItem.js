dbdf09d56835670985d00da4878bdc51685f5aa8
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-01-20 03:17:00 -0700

    Passing data via 'props'

--- React Basics and Working with Components/expense-tracker/src/components/ExpenseItem.js
+++ React Basics and Working with Components/expense-tracker/src/components/ExpenseItem.js
@@ -1,18 +1,15 @@
 import "./ExpenseItem.css";
 //A component in react IS a JS function
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 0, 20);
-  const expenseTitle = "Fall Guys Skin";
-  const expenseAmount = 4;
+function ExpenseItem(props) {
   //Its not allowed to have 2 or more root elements in a jsx return statement
   //Fix that by enclosing everything in a single root element, for example a single div
   return (
     //its className cause its jsx, not regular html which would use class
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
