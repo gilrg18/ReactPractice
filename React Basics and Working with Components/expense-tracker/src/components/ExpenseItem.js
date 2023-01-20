c477d280893b9b945bf6e3671b07dfac7b5674d4
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-01-20 02:55:47 -0700

    Adding Basic CSS Styling

--- React Basics and Working with Components/expense-tracker/src/components/ExpenseItem.js
+++ React Basics and Working with Components/expense-tracker/src/components/ExpenseItem.js
@@ -1,13 +1,15 @@
+import './ExpenseItem.css';
 //A component in react IS a JS function
 function ExpenseItem() {
   //Its not allowed to have 2 or more root elements in a jsx return statement
   //Fix that by enclosing everything in a single root element, for example a single div
   return (
-    <div>
+    //its className cause its jsx, not regular html which would use class
+    <div className="expense-item">
       <div>May 18th 2023</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Skins</h2>
-        <div>$4.00</div>
+        <div className="expense-item__price">$4.00</div>
       </div>
     </div>
   );
