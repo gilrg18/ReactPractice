55250e1887f83fb10b9d2fa9ee1a5cf68f3d5ab6
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-01-20 03:51:28 -0700

    Splitting components into multiple components

--- React Basics and Working with Components/expense-tracker/src/components/ExpenseItem.js
+++ React Basics and Working with Components/expense-tracker/src/components/ExpenseItem.js
@@ -1,20 +1,15 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 //A component in react IS a JS function
 function ExpenseItem(props) {
-    const month = props.date.toLocaleString("en-US", { month: "long" });
-    const day = props.date.toLocaleString("en-US", {day: '2-digit'});
-    const year = props.date.getFullYear();
+    
 
   //Its not allowed to have 2 or more root elements in a jsx return statement
   //Fix that by enclosing everything in a single root element, for example a single div
   return (
     //its className cause its jsx, not regular html which would use class
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
