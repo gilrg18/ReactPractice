3b07402ae552756d47bb3d8036ce997b77b6768c
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-01-20 02:51:24 -0700

    Writing more complex JSX code

--- React Basics and Working with Components/expense-tracker/src/components/ExpenseItem.js
+++ React Basics and Working with Components/expense-tracker/src/components/ExpenseItem.js
@@ -1,6 +1,16 @@
 //A component in react IS a JS function
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+  //Its not allowed to have 2 or more root elements in a jsx return statement
+  //Fix that by enclosing everything in a single root element, for example a single div
+  return (
+    <div>
+      <div>May 18th 2023</div>
+      <div>
+        <h2>Skins</h2>
+        <div>$4.00</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
