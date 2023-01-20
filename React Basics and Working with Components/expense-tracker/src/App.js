7f5934c25a4083b01703117ab042b07f7a10c3df
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-01-20 02:38:36 -0700

    How React Works (Declarative Way)

--- React Basics and Working with Components/expense-tracker/src/App.js
+++ React Basics and Working with Components/expense-tracker/src/App.js
@@ -1,7 +1,14 @@
 function App() {
+  //imperative way:
+  //const para = document.createElement('p');
+  //para.textContent = 'Hello';
+  //document.getElementById('root').append(para);
+  //alot of instructions for the DOM, this is the difference between vanilla JS and ReactJS
   return (
+    //declarative way:
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Let's get started woo!</h2>
+      <p>Hello </p>
     </div>
   );
 }
