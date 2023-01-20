2f5d95a3fc7c3b05008d2be6e33c08e696599fab
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-01-20 03:06:40 -0700

    Outputting Dynamic Data and Working with Expressions in JSX

--- React Basics and Working with Components/expense-tracker/src/components/ExpenseItem.js
+++ React Basics and Working with Components/expense-tracker/src/components/ExpenseItem.js
@@ -1,15 +1,18 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 //A component in react IS a JS function
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 0, 20);
+  const expenseTitle = "Fall Guys Skin";
+  const expenseAmount = 4;
   //Its not allowed to have 2 or more root elements in a jsx return statement
   //Fix that by enclosing everything in a single root element, for example a single div
   return (
     //its className cause its jsx, not regular html which would use class
     <div className="expense-item">
-      <div>May 18th 2023</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Skins</h2>
-        <div className="expense-item__price">$4.00</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
