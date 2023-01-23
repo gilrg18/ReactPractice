0071672983d84ea9d8a0ce625e124bcdfa731722
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-01-23 03:32:05 -0700

    Working with state

--- React Basics and Working with Components/expense-tracker/src/components/Expenses/ExpenseItem.js
+++ React Basics and Working with Components/expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,32 @@
+import { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 //A component in react IS a JS function
 const ExpenseItem = (props) => {
+  //destructuring - useState returns the value and a function to update the value
+  //usestate receives the initial value as parameter.
+  const [title, setTitle] = useState(props.title);
+
+  //let title = props.title;
   //document.getElementById('root').addEventListener()
   //onClick prop adds and event listener to the <button> in a declarative way
+  // const clickHandler = () => {
+  //   //this doesnt work because the returned JSX needs to be reevaluated so the component
+  //   //can be updated, for the component to be updated we use "useState from the react library"
+  //   title = 'Updated!!';
+  //   console.log('updated!!');
+  // };
+
   const clickHandler = () => {
-    console.log("Clicked!!!");
-  };
+    //useState tells react that the component should be reevaluated and updates the component
+    setTitle('Updated!!');
+    //console log will print the first title first because setTitle doesnt change the value right away,
+    //it schedules the state update
+    console.log(title);
+  }
+
   //onClick={clickHandler} you pass it as a value, if you pass it as a function (clickHandler())
   //it will execute when the jsx gets rendered
 
@@ -18,7 +37,7 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
