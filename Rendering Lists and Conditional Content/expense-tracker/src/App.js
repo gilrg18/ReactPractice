8a0e604cc5b4fdcbfa8a50b27c0570826621d76a
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-01-25 04:22:01 -0700

    Using Stateful Lists

--- Rendering Lists and Conditional Content/expense-tracker/src/App.js
+++ Rendering Lists and Conditional Content/expense-tracker/src/App.js
@@ -1,9 +1,8 @@
-import React from 'react'; //React is always being used under the hood when using JSX code
+import React, {useState} from 'react'; //React is always being used under the hood when using JSX code
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from './components/NewExpense/NewExpense';
 
-const App = () => {
-  const expenses = [
+  const DUMMY_EXPENSES = [
     {
       id: "e1",
       title: "Toilet Paper",
@@ -25,7 +24,13 @@ const App = () => {
     },
   ];
 
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const addExpenseHandler = expense => {
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
     console.log('In App.js');
     console.log(expense);
   }
