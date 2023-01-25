729797221d3217ef5d7ea44082fe4eafb917f9d6
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-01-25 04:42:56 -0700

    Using year to filter expense items

--- Rendering Lists and Conditional Content/expense-tracker/src/components/Expenses/Expenses.js
+++ Rendering Lists and Conditional Content/expense-tracker/src/components/Expenses/Expenses.js
@@ -11,6 +11,11 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
     console.log(selectedYear);
   };
+
+  const filteredExpenses = props.expenseList.filter(expense=>{
+    return expense.date.getFullYear().toString === filteredYear;
+  })
+
   return (
     <div>
       <Card className="expenses">
@@ -19,12 +24,12 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
         {/* Render Lists of Data using Array.map() method */}
-        {props.expenseList.map((expense) => (
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
           //You must add a key value, otherwise all items will be updated since the new item will be 
           //added to the bottom of the list in the browser dom but in the top of the list in react so react
           //will try and fix this by updating all items which could lead to bugs and performance issues
-          //This happens because thats how react works.. we beed a key prop to avoid these issues
+          //This happens because thats how react works.. we beed a unique key prop to avoid these issues
             key={expense.id}
             date={expense.date}
             amount={expense.amount}
