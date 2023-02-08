12dd4937c81b2b5780b229976a6cef9f373527bc
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-02-08 15:27:29 -0700

    Style components & media queries

--- StylingReactComponentsMiniProject/src/components/UI/Button/Button.js
+++ StylingReactComponentsMiniProject/src/components/UI/Button/Button.js
@@ -10,6 +10,7 @@ const Button = styled.button`
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
 
+  //se le quita el auto abajo de 768px
   @media (min-width: 768px) {
     width: auto;
   }
