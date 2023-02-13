bfe8f914a529a9569bd36f22659abce386084c14
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-02-13 00:28:46 -0700

    Managing the User Input State

--- Practice Project - Adding Users/src/components/Users/AddUser.js
+++ Practice Project - Adding Users/src/components/Users/AddUser.js
@@ -1,26 +1,37 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
-import classes from './AddUser.module.css'
+import classes from "./AddUser.module.css";
 
-const AddUser = props => {
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
 
-    const addUserHandler = (event) => {
-        //Prevents default reload when submitting a form which can mess with react state
-        event.preventDefault();
-        
-    }
+  const addUserHandler = (event) => {
+    //Prevents default reload when submitting a form which can mess with react state
+    event.preventDefault();
+    console.log(enteredUsername, enteredAge);
+  };
 
-    return (
-      <Card className={classes.input}>
-        <form onSubmit={addUserHandler}>
-          <label htmlFor="username">Username</label>
-          <input id="username" type="text" />
-          <label htmlFor="age">Age (Years)</label>
-          <input id="age" type="number" />
-          <Button type="submit">Add User</Button>
-        </form>
-      </Card>
-    );
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" onChange={ageChangeHandler} />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
 };
 
 export default AddUser;
