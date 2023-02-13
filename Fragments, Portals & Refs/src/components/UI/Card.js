eb2f50f742a6cfc99b52195c5909ff5ac9b7460c
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-02-13 03:11:01 -0700

    Deleted shit

--- Fragments, Portals & Refs/src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-import classes from './Card.module.css'
-
-const Card = props => {
-    //using template literals `` to add more than one className
-    return <div className={`${classes.card} ${props.className}`}>{props.children}</div>
-};
-
-export default Card;
