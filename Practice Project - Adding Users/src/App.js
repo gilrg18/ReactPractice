e08df29c8facc69deb5f0608c346013c868cd05d
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-02-13 01:44:37 -0700

    Adding a Users List component

--- Practice Project - Adding Users/src/App.js
+++ Practice Project - Adding Users/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 
 function App() {
   return (
     <div>
       <AddUser/>
+      <UsersList users={[{name:'gil', age:26},{name:'cobi', age:14}]}/>
     </div>
   );
 }
