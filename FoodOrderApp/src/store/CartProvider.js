307bfd9a5ed6220db886537b51de816801f0cf17
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-02-27 18:04:55 -0700

    Adding a cart REDUCER

--- FoodOrderApp/src/store/CartProvider.js
+++ FoodOrderApp/src/store/CartProvider.js
@@ -1,19 +1,54 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+//reducer function goes outside of the component because it doesnt need any data from it
+//state is the last state snapshot of the state managed by the reducer
+const cartReducer = (state, action) => {
+  if (action.type === "ADD"){
+    //concat returns a total new array, different from push which works with the current array
+    //which is better for react because of how state works
+    const updateItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+    return {
+        items:updateItems,
+        totalAmount: updatedTotalAmount
+    };
+  }
+    //return a new state snapshot
+    return defaultCartState;
+};
+
 const CartProvider = (props) => {
-    const addItemToCartHandler = item =>{};
-    const removeItemFromCartHandler = id =>{};
+  //useReducer(point at the reducer function, set an initial state)
+  //use reducer returns an array with two elements [state snapshot, function to dispatch (enviar) an action to the reducer]
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'ADD', item: item }); //you can name the variables whatever you want fe: identifier: 'ADD'
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({type:'REMOVE', id: id})
+  };
 
-    const cartContext = {
-        items: [],
-        totalAmount: 0,
-        addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler
-    }
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
 
-    return <CartContext.Provider value={cartContext}>
-        {props.children}
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
     </CartContext.Provider>
-}
+  );
+};
 
 export default CartProvider;
