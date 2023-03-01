fb2bfaff4b7076ea05a8394c4028fa22a3c2a341
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-02-28 18:43:50 -0700

    Use effect to add bouncy effect to cart button

--- FoodOrderApp/src/components/Layout/HeaderCartButton.js
+++ FoodOrderApp/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,44 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
+  //destructuring just the items from the context
+  const { items } = cartCtx;
   //now this component will get reevaluated whenver the CartContext changes
   //it also changes when it gets updated in the CartProvider component
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item)=> {
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    //create new timer
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300); //300 miliseconds because thats the duration of the bump css class
+
+    //cleanup function
+    return () => {
+      clearTimeout(timer); //clear old timer
+      console.log(items);
+    };
+  }, [items]);
+
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
