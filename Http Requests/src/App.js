ba57ef510e013506e0e82e58e761fecd1b7264ff
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-02 16:25:45 -0700

    Replacing .then() with async / await

--- Http Requests/src/App.js
+++ Http Requests/src/App.js
@@ -5,26 +5,22 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-
-  function fetchMoviesHandler() {
+  //Replacing .then calls with async await for readability purposes (works the same)
+  async function fetchMoviesHandler() {
     //the default method is GET
-    //fetch returns a promise which is an object with data
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        //.results is a field in the api
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date
-          };
-        });
-        setMovies(transformedMovies);
-      });
+    //fetch returns a PROMISE which is an object with data
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    //.results is a field in the api
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
 
   return (
