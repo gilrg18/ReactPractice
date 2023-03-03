1e9746a8ff30232c229bc00864b320f7e1048cd1
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-02 22:14:14 -0700

    useEffect to request on load and useCallback to avoid unnecesary re-renders

--- Http Requests/src/App.js
+++ Http Requests/src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
@@ -7,24 +6,17 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  //Replacing .then calls with async await for readability purposes (works the same)
-  async function fetchMoviesHandler() {
+
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      //the default method is GET
-      //fetch returns a PROMISE which is an object with data
-      //bad url for error test: https://swapi.dev/api/film/
-      //good url https://swapi.dev/api/films/
       const response = await fetch("https://swapi.dev/api/films/");
-      //the response also has a "status" field which holds the concrete response
-      //status code. You could also manually check that.
       if (!response.ok) {
         throw new Error("Something went wrong!!!");
       }
       const data = await response.json();
 
-      //.results is a field in the api
       const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
@@ -38,20 +30,38 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+  //its a good practice to add all used dependencies inside useEffect to the dependencies array
+  //functions are objects that will change everytime the component rerenders, so it will create
+  //an infinite loop, we can fix it with useCallback hook
+  /*
+  useCallback is mostly used when you don't want a function to get un-necessarily 
+  created each time on every render and subsequent re-renders of the component.
+  Remember that useCallback will always return the same instance of the function on 
+  re-renders unlike normal functions where they are recreated with each re-render of the component. 
+  The useCallback would refresh only when there is a change in dependencies.
+  The second argument of useCallback refers to the dependencies which would trigger a 
+  re-initialization of the function defined inside useCallback if those dependencies change.
+  In this example the useCallback function ( fetchMoviesHandler ) doesnt have any dependencies so 
+  we leave an empty []
+  */
 
   let content = <p>Found no movies.</p>;
 
-  if(movies.length > 0){
-    content = <MoviesList movies = {movies}/>;
-  }
-  
-  if(error){
-    content = <p>{error}</p>
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
   }
 
-  if(isLoading){
-    content = <p>Loading...</p>
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -59,9 +69,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-       {content}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
