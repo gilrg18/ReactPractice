3dd3c2f970d8564fbac2e9be7b62c855e575c2c4
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-02 21:51:48 -0700

    Handling http errors

--- Http Requests/src/App.js
+++ Http Requests/src/App.js
@@ -6,24 +6,52 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   //Replacing .then calls with async await for readability purposes (works the same)
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    //the default method is GET
-    //fetch returns a PROMISE which is an object with data
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    //.results is a field in the api
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    setError(null);
+    try {
+      //the default method is GET
+      //fetch returns a PROMISE which is an object with data
+      //bad url for error test: https://swapi.dev/api/film/
+      //good url https://swapi.dev/api/films/
+      const response = await fetch("https://swapi.dev/api/films/");
+      //the response also has a "status" field which holds the concrete response
+      //status code. You could also manually check that.
+      if (!response.ok) {
+        throw new Error("Something went wrong!!!");
+      }
+      const data = await response.json();
+
+      //.results is a field in the api
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if(movies.length > 0){
+    content = <MoviesList movies = {movies}/>;
+  }
+  
+  if(error){
+    content = <p>{error}</p>
+  }
+
+  if(isLoading){
+    content = <p>Loading...</p>
   }
 
   return (
@@ -32,9 +60,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {isLoading && <p>Loading...</p>}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        {!isLoading && movies.length> 0 && <MoviesList movies={movies} />}
+       {content}
       </section>
     </React.Fragment>
   );
