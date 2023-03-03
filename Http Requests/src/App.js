bd96d7a506ceb5dbd4a1db66e24004cb183ab797
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-02 23:58:44 -0700

    Creating a firebase db/api and Setting up project

--- Http Requests/src/App.js
+++ Http Requests/src/App.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect, useCallback } from "react";
-import MoviesList from "./components/MoviesList";
-import "./App.css";
+import React, { useState, useEffect, useCallback } from 'react';
+
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
+import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -11,10 +13,13 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-http-practice-56e55-default-rtdb.firebaseio.com/movies.json"
+      ); //URL from firebase react-http-practice
       if (!response.ok) {
-        throw new Error("Something went wrong!!!");
+        throw new Error('Something went wrong!');
       }
+
       const data = await response.json();
 
       const transformedMovies = data.results.map((movieData) => {
@@ -35,20 +40,10 @@ function App() {
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
-  //its a good practice to add all used dependencies inside useEffect to the dependencies array
-  //functions are objects that will change everytime the component rerenders, so it will create
-  //an infinite loop, we can fix it with useCallback hook
-  /*
-  useCallback is mostly used when you don't want a function to get un-necessarily 
-  created each time on every render and subsequent re-renders of the component.
-  Remember that useCallback will always return the same instance of the function on 
-  re-renders unlike normal functions where they are recreated with each re-render of the component. 
-  The useCallback would refresh only when there is a change in dependencies.
-  The second argument of useCallback refers to the dependencies which would trigger a 
-  re-initialization of the function defined inside useCallback if those dependencies change.
-  In this example the useCallback function ( fetchMoviesHandler ) doesnt have any dependencies so 
-  we leave an empty []
-  */
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
 
   let content = <p>Found no movies.</p>;
 
@@ -66,6 +61,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
