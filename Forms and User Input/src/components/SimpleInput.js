7596543b2091352628b8249f315b60bc560fd445
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-06 18:43:38 -0700

    Providing Validation Feedback

--- Forms and User Input/src/components/SimpleInput.js
+++ Forms and User Input/src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
   //useState for validation and to reset / useRef cant do this
   //useRef if you only want to use the data once
   const nameInputChangeHandler = (event) => {
@@ -13,21 +14,23 @@ const SimpleInput = (props) => {
     event.preventDefault();
     //validation in the front end is just for user experience since it can be accesed and hacked by the clientside users
     //validation should be programmed in the backend
-    if(enteredName.trim() == ''){
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
       return;
     }
+    setEnteredNameIsValid(true);
     console.log(enteredName);
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
     //nameInputRef.current.value=''; //you shouldnt manipulate the DOM directly like this, react should be the one manipulating the DOM
-    setEnteredName('');
+    setEnteredName("");
   };
 
-  
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -36,6 +39,7 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
+        {!enteredNameIsValid && <p className='error-text'>Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
