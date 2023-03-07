3de141290bfb1386701914a94e7d6a8d27482a11
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-06 21:09:54 -0700

    Refactoring and deriving states

--- Forms and User Input/src/components/SimpleInput.js
+++ Forms and User Input/src/components/SimpleInput.js
@@ -1,27 +1,23 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
   //useState for validation and to reset / useRef cant do this
   //useRef if you only want to use the data once
-
+  
+  //deriving state from enteredName
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
-    //we cant use enteredName because the scheduling doesnt update the state instantly
-    //so we use event.target.value
-    if (event.target.value.trim() !== "") {
-      setEnteredNameIsValid(true);
-    }
+
   };
 
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);  
-    }
+
   };
 
   const formSubmissionHandler = (event) => {
@@ -31,19 +27,16 @@ const SimpleInput = (props) => {
 
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
-    setEnteredNameIsValid(true);
+    
     console.log(enteredName);
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
     //nameInputRef.current.value=''; //you shouldnt manipulate the DOM directly like this, react should be the one manipulating the DOM
     setEnteredName("");
+    setEnteredNameTouched(false);
   };
 
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputClasses = nameInputIsInvalid
     ? "form-control invalid"
@@ -54,7 +47,6 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           onBlur={nameInputBlurHandler} //whenever the input looses focus
