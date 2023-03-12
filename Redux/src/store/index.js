4786d62e8f3d0065a1d0b3f21a7084088615ac5c
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-12 02:27:12 -0700

    Connecting Redux Toolkit State using configureStore

--- Redux/src/store/index.js
+++ Redux/src/store/index.js
@@ -1,73 +1,81 @@
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
 //Creating this to avoid typo problems in the future
-export const INCREMENT = 'increment';
+export const INCREMENT = "increment";
 
 //preparing a slice of our global state
 //we can create different slices in different files to make our code maintainable
-createSlice({
-  name: 'counter',
+const counterSlice = createSlice({
+  name: "counter",
   initialState: initialState,
   reducers: {
-    increment(state){
+    increment(state) {
       //we shouldnt manipulate the state directly like this but redux toolkit takes care of it and allows it
       state.counter++;
     },
-    decrement(state){
+    decrement(state) {
       state.counter--;
     },
-    increase(state, action){
+    increase(state, action) {
       state.counter = state.counter + action.qty;
     },
-    toggle(state){
+    toggle(state) {
       state.showCounter = !state.showCounter;
     },
-  }
+  },
 });
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === INCREMENT) {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-    //redux dont merge the new state with the existing state, it overwrites (replaces) it
-    //so we have to tell redux to create every propery of the state otherwise it will delete
-    //in this case showCounter if we dont add it to the new state
-    //We must always set all pieces of state when we update a piece of state
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === INCREMENT) {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//     //redux dont merge the new state with the existing state, it overwrites (replaces) it
+//     //so we have to tell redux to create every propery of the state otherwise it will delete
+//     //in this case showCounter if we dont add it to the new state
+//     //We must always set all pieces of state when we update a piece of state
 
-    //state.counter++
-    //return state;
-    //this works but you should NEVER mutate exisiting state, this could lead to bugs and unpredictable behaviour
-  }
+//     //state.counter++
+//     //return state;
+//     //this works but you should NEVER mutate exisiting state, this could lead to bugs and unpredictable behaviour
+//   }
 
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.qty,
-      showCounter: state.showCounter,
-    };
-  }
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.qty,
+//       showCounter: state.showCounter,
+//     };
+//   }
 
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
 
-  if (action.type === "toggle") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
+//   if (action.type === "toggle") {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     };
+//   }
 
-  return state;
-};
+//   return state;
+// };
 
-const store = createStore(counterReducer);
+// const store = createStore(counterReducer);
+//configureStore makes merging multiple reducers into one, easier
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
+//alternitavely we can set a key(counter) to the reducer function(counterSlice.reducer)
+//this is used when we have multiple reducers
+// const store = configureStore({
+//   reducer: { counter: counterSlice.reducer },
+// });
 export default store;
