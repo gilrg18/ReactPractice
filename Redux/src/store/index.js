bd8cbac30bc0fa60a7571bef2db1999cfad88345
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-11 23:41:43 -0700

    Providing the store and fixed counterReducer

--- Redux/src/store/index.js
+++ Redux/src/store/index.js
@@ -1,15 +1,19 @@
 import { createStore } from "redux";
 
-const counterReducer =(state = { counter: 0 }, action) => {
-    if(action.type === 'increment'){
-        counter = state.counter + 1;
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
     };
-    if(action.type === 'decrement'){
-        counter = state.counter - 1;
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
     };
+  }
 
-    return state;
-}
+  return state;
+};
 
 const store = createStore(counterReducer);
 
