3f6f79a7d1c6199d4b796da9e5944fb56de7cc28
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-12 23:59:20 -0700

    Writing our first test

---
+++ Unit Tests/src/components/Greeting.test.js
@@ -0,0 +1,17 @@
+import Greeting from "./Greeting";
+import { render, screen } from "@testing-library/react";
+//screen is some kind of virtual dom that is rendered for testing
+test('renders Hello World as a text', () => {
+    //The three A's
+  //Arrange: Set up the test data, test conditions and test environment
+  render(<Greeting />);
+  //Act: Run logic that should be tested(e.g execute a function)
+  //Assert: Compare execution results with expected results
+  const helloWorldElement = screen.getByText('Hello World', {exact: false})//true is the default, you shouldnt add it to find the exact words
+  //Test passes with exact: false because Greeting has Hello World! rendered and Hello World by itself is not exactly Hello World!
+  expect(helloWorldElement).toBeInTheDocument();
+}
+
+
+
+)
