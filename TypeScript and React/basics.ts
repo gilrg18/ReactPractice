1540514778d7f908a36706f84cc8e24ed890d4e3
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-14 21:59:11 -0700

    Array and Object types

--- TypeScript and React/basics.ts
+++ TypeScript and React/basics.ts
@@ -3,12 +3,32 @@
 //Function types, parameters
 
 //Primitives:
-
 let age: number = 18; //number is primitive, Number is the NUMBER object
 age =18.8;
-age = '123';
+//age = '123';
 
 let username: string = 'Gil';
 
 let isStudent: boolean = true;
 isStudent = false;
+
+//Arrays and Objects
+let hobbies: string[]; //string array
+hobbies = ['Sports', 'Cooking', 'VideoGames']
+
+//let person: any; //any type is basically going back to standar javascript
+let person: {
+    name: string;
+    age: number;
+};
+
+person = {
+    name: 'Gil',
+    age: 18,
+}
+
+let people: {
+  name: string;
+  age: number;
+}[]; //array of objects
+
