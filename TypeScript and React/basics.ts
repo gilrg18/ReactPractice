12a7d541cb6b01a3c3ede7f2c73541002cc5e66a
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-14 21:52:42 -0700

    number, string and boolean types

---
+++ TypeScript and React/basics.ts
@@ -0,0 +1,14 @@
+//Primitives: number, string, boolean
+//More complex types: arrays,objects
+//Function types, parameters
+
+//Primitives:
+
+let age: number = 18; //number is primitive, Number is the NUMBER object
+age =18.8;
+age = '123';
+
+let username: string = 'Gil';
+
+let isStudent: boolean = true;
+isStudent = false;
