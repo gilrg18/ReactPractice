bd7d50322067f76d98c9cc9f0fbef89de8f9b0d0
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-14 18:30:46 -0700

    Testing async code with MOCK data (ideal for http requests)

--- Unit Tests/src/components/Async.js
+++ Unit Tests/src/components/Async.js
@@ -4,6 +4,7 @@ const Async = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
+   
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((data) => {
--- Unit Tests/src/components/Async.test.js
+++ Unit Tests/src/components/Async.test.js
@@ -4,10 +4,20 @@ import Async from './Async';
 describe('Async component', ()=>{
 
     test('render posts if request succeeds', async ()=>{
-        render(<Async/>)
-        //getAllByRole to get all fetched data however it wont work cause of asynchronous data
-        //findAllByRole will work because it returns a PROMISE so u have to add async and await
-        const listItemElements = await screen.findAllByRole('listitem'); //('listitem',{exact:..},{timeout})
-        expect(listItemElements).not.toHaveLength(0);
+      //dummy function that mocks a request so we dont saturate the server with http requests: jest.fn
+      window.fetch = jest.fn();
+      //resolvedValue is .then((response) => response.json()) in Async.js
+      window.fetch.mockResolvedValueOnce({
+        json: async () => [{id: 'p1', title: 'First post'}],
+      });
+      render(<Async />);
+      //getAllByRole to get all fetched data however it wont work cause of asynchronous data
+      //findAllByRole will work because it returns a PROMISE so u have to add 'async' and 'await'
+      const listItemElements = await screen.findAllByRole("listitem"); //('listitem',{exact:..},{timeout})
+      expect(listItemElements).not.toHaveLength(0);
     });
 })
+
+//During testing you dont wanna saturate your server with requests or even post request that will
+//fill your database with data. 
+//Therefore we either send a fake request or send a request to a fake/test server
