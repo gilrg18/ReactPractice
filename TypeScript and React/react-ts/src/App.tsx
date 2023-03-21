1ea586c75ef2d78cd004d080bc83928dfc5a92db
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-21 15:04:47 -0700

    Managing State and typescript

--- TypeScript and React/react-ts/src/App.tsx
+++ TypeScript and React/react-ts/src/App.tsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 //npx create-react-app react-ts --template typescript
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const onAddTodoHandler = (text: string): void => {
+    const newTodo = new Todo(text);
 
-  const onAddTodo = (text: string): void => {
-    return;
+    setTodos((prevTodos) => {
+      //concat creates a new array (it doesnt mutate the existing array)
+      return prevTodos.concat(newTodo);
+    });
   };
 
   return (
     <div>
-      <NewTodo onAddTodo={onAddTodo} />
+      <NewTodo onAddTodo={onAddTodoHandler} />
       <Todos items={todos}></Todos>
     </div>
   );
