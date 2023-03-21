f1f980ad2697c3b40471a765e2ecde9d65bf7394
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-21 09:50:26 -0700

    More props with typescript

--- TypeScript and React/react-ts/src/components/Todos.tsx
+++ TypeScript and React/react-ts/src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
 //FC = Functional Component
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} todoText={item.text}></TodoItem>
       ))}
     </ul>
   );
