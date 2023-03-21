1b3460d99b210863b275891c0ffb767f3a4ebdf6
Gilberto Rogel <gilberto_rogel_26@hotmail.com>
2023-03-21 15:10:16 -0700

    Added styling

--- TypeScript and React/react-ts/src/components/NewTodo.tsx
+++ TypeScript and React/react-ts/src/components/NewTodo.tsx
@@ -1,23 +1,24 @@
 import { Ref, useRef } from "react";
+import classes from "./NewTodo.module.css";
+
 //onAddTodo is a function that returns nothing (void) and gets one argument (text of type string)
-const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
-    const todoTextInputRef = useRef<HTMLInputElement>(null);
-    const submitHandler = (event: React.FormEvent) => {
-        event.preventDefault();
-        //! - if you are certain that it cant be null
-        const enteredText = todoTextInputRef.current!.value;
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    //! - if you are certain that it cant be null
+    const enteredText = todoTextInputRef.current!.value;
 
-        if(enteredText.trim().length === 0){
-            //throw error
-            return;
-        }
-
-        props.onAddTodo(enteredText);
-
+    if (enteredText.trim().length === 0) {
+      //throw error
+      return;
     }
 
+    props.onAddTodo(enteredText);
+  };
+
   return (
-    <form onSubmit={submitHandler}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
       <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
